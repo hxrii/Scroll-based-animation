1c65eb8d20bfff8bf406e15868e66bbadb6d0b9f
Hari <mail.hariiiiiii@gmail.com>
2024-09-03 12:43:58 +0530

    Added all animations

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import GUI from 'lil-gui'
-
+import gsap from 'gsap'
 /**
  * Debug
  */
@@ -171,6 +171,30 @@ addEventListener('mousemove',(e)=>{
 
 
 
+
+let scrollY = window.scrollY
+let currentSection = 0
+
+
+window.addEventListener('scroll', () =>
+    {
+        scrollY = window.scrollY
+    
+        const newSection = Math.round(scrollY / sizes.height)
+        if(newSection != currentSection)
+            {
+                currentSection = newSection
+        
+                gsap.to(sectionMeshes[currentSection].rotation,{
+                    duration: 1.5,
+                    ease: 'power2.inOut',
+                    x: '+=6',
+                    y: '+=3',
+                    z: '+=1.5'
+                })
+            }
+    })
+
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
@@ -188,8 +212,8 @@ const tick = () =>
 
     for(const mesh of sectionMeshes)
         {
-            mesh.rotation.x = elapsedTime * 0.1
-            mesh.rotation.y = elapsedTime * 0.12
+            mesh.rotation.x += deltaTime * 0.1
+            mesh.rotation.y += deltaTime * 0.12
         }
 
     // Animate camera
