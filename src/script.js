e5a0d19c2881a91e151352d80e92b42d27512cba
Hari <mail.hariiiiiii@gmail.com>
2024-09-03 12:34:58 +0530

    Added particles

--- src/script.js
+++ src/script.js
@@ -36,7 +36,10 @@ gui
     .onChange(() =>
         {
             toonMaterial.color.set(parameters.materialColor)
+            particlesMaterial.color.set(parameters.materialColor)
         })
+
+
 
 
 
@@ -67,6 +70,44 @@ scene.add(light)
 
 
 
+
+
+
+//Mesh positions
+const objectsDistance = 4
+
+mesh1.position.y = - objectsDistance * 0
+mesh2.position.y = - objectsDistance * 1
+mesh3.position.y = - objectsDistance * 2
+
+
+const sectionMeshes = [ mesh1, mesh2, mesh3 ]
+
+
+
+
+//paricles
+const particleCnt = 1000;
+const positions = new Float32Array(particleCnt*3);
+for(let i = 0; i < particleCnt; i++)
+    {
+        positions[i * 3 + 0] = (Math.random() - 0.5) * 10
+        positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length
+        positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+    }
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+
+const particlesMaterial = new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    sizeAttenuation: true,
+    size: 0.03
+})
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
 /**
  * Sizes
  */
@@ -94,9 +135,13 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+
+// Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
@@ -112,14 +157,50 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0;
+
+const cursor = {}
+cursor.x = 0;
+cursor.y =0;
+
+
+addEventListener('mousemove',(e)=>{
+    cursor.x = e.clientX / sizes.width -0.5
+    cursor.y = e.clientY / sizes.height -0.5
+})
+
+
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
+    const deltaTime = elapsedTime-previousTime
+    previousTime = elapsedTime
     // Render
     renderer.render(scene, camera)
+    
 
+    let scrollY = window.scrollY
+    camera.position.y = - scrollY / sizes.height * objectsDistance
+    mesh1.position.x = 2
+    mesh2.position.x = - 2
+    mesh3.position.x = 2
+
+    for(const mesh of sectionMeshes)
+        {
+            mesh.rotation.x = elapsedTime * 0.1
+            mesh.rotation.y = elapsedTime * 0.12
+        }
+
+    // Animate camera
+    camera.position.y = - scrollY / sizes.height * objectsDistance
+
+    const parallaxX = cursor.x /2
+    const parallaxY = - cursor.y /2
+    
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * deltaTime*5;
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * deltaTime*5;
+    
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
